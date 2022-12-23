5176a9c54b875354c653fe86b11a0cdcf661a28f
Evhen Pavelko <lomonosov213@i.ua>
2022-12-23 11:46:46 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -101,10 +101,10 @@ titleCaseOver = 9000;
 let catName = "Oliver";
 let catSound = "Meow!";
 //////////////////////////
-const fCC = "freeCodeCamp"; // Change this line
+const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(fCC, fact); // Change this line
+console.log(FCC, fact); // Change this line
 
 //========================================================================================================================================================
 
