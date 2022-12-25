9474438284df64fbb5ccd5285547958fb7c22e1e
Evhen Pavelko <lomonosov213@i.ua>
2022-12-25 22:17:45 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -117,6 +117,33 @@ let myVar = 87;
 myVar++;
 //////////////////
 const ourDecimal = 5.7;
+///////////
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+////////////////
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+//////////////
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= b;
+c *= 10;
 
 // Only change code below this line
 const myDecimal = 5.7;
