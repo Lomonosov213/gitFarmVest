757c2aab2e236ee8f01f722abf3ea432a21e221c
Evhen Pavelko <lomonosov213@i.ua>
2022-12-26 20:28:43 +0100

    comit

--- src/js/app.js
+++ src/js/app.js
@@ -285,6 +285,39 @@ function trueOrFalse(wasThatTrue) {
   return "No, that was false";
 }
 /////////
+// Setup
+function testEqual(val) {
+  if (val == 12) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testEqual(10);
+//////////////////
+function testGreaterThan(val) {
+  if (val > 100) {
+    // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {
+    // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
+testGreaterThan(0);
+testGreaterThan(11);
+testGreaterThan(99);
+testGreaterThan(100);
+testGreaterThan(101);
+testGreaterThan(150);
+////////////////
 
 //========================================================================================================================================================
 
