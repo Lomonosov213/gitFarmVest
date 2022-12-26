9c097bfe9d70cb5f5d4f4f73f1d80dda5329985d
Evhen Pavelko <lomonosov213@i.ua>
2022-12-26 18:33:31 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -226,6 +226,28 @@ timesFive(5);
 timesFive(2);
 timesFive(0);
 /////////////
+// Declare the myGlobal variable below this line
+const myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+///////////////
+
 //========================================================================================================================================================
 
 /* Надсилання форми */
