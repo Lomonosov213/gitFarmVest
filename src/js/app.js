b1c93b3b41a90fda5a1cd5397f147854dc089b7c
Evhen Pavelko <lomonosov213@i.ua>
2022-12-26 19:32:11 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -247,6 +247,36 @@ function fun2() {
   console.log(output);
 }
 ///////////////
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+///////////
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  const removed = arr.shift();
+  return removed;
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log(nextInLine([2], 1));
+console.log(nextInLine([5, 6, 7, 8, 9], 1));
+console.log("After: " + JSON.stringify(testArr));
+//////////
 
 //========================================================================================================================================================
 
