0a15d29370257eb8da0fba035bf5480a43aab59f
Evhen Pavelko <lomonosov213@i.ua>
2022-12-27 21:01:17 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -373,6 +373,33 @@ function testLogicalOr(val) {
 
 testLogicalOr(15);
 ////////////////
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+////////////
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  } else if (val < 5) {
+    return "Smaller than 5";
+  } else {
+    return "Between 5 and 10";
+  }
+}
+
+testElseIf(7);
 
 //========================================================================================================================================================
 
