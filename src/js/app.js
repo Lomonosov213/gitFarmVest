085e67b0af5fea3ed46c34c36175f409ac37e001
Evhen Pavelko <lomonosov213@i.ua>
2023-01-02 12:37:24 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -641,6 +641,27 @@ const myDog = {
 };
 
 myDog.bark = "gow";
+///////////////
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
+    alpha: "Adams",
+    bravo: "Boston",
+    charlie: "Chicago",
+    delta: "Denver",
+    echo: "Easy",
+    foxtrot: "Frank",
+  };
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
 
 //========================================================================================================================================================
 
