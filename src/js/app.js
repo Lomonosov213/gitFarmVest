48222a2a96f604535a8284cfa8577b5f9ce2fe1f
Evhen Pavelko <lomonosov213@i.ua>
2023-01-07 20:53:55 +0100

    post

--- src/js/app.js
+++ src/js/app.js
@@ -720,6 +720,33 @@ var myMusic = [
   },
 ];
 ///////////////
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
+  },
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
+/////////////
+const myPlants = [
+  {
+    type: "flowers",
+    list: ["rose", "tulip", "dandelion"],
+  },
+  {
+    type: "trees",
+    list: ["fir", "pine", "birch"],
+  },
+];
+
+const secondTree = myPlants[1].list[1];
+///////////////
 
 //========================================================================================================================================================
 
