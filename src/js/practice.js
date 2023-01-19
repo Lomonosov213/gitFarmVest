87483a622fe69cc7d39b0b77bb207d2522800242
Evhen Pavelko <lomonosov213@i.ua>
2023-01-19 11:30:48 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -2,7 +2,22 @@ function checkAge(age) {
   if (age > 18) {
     return true;
   } else {
-    return "Родители разрешили?";
+    return confirm('Родители разрешили?');
   }
 }
-console.log(checkAge(19));
+
+function convertToRoman (num) {
+    if (isNaN(num))
+        return NaN;
+    var digits = String(+num).split(""),
+        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+        roman = "",
+        i = 3;
+    while (i--)
+        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+    return Array(+digits.join("") + 1).join("M") + roman;
+}
+
+convertToRoman(36);
