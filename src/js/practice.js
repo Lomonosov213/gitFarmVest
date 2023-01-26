433dd6b7f9a61b47b448bf77404a41119cd6c759
Evhen Pavelko <lomonosov213@i.ua>
2023-01-26 21:40:48 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -138,3 +138,12 @@ function twoSumm(arr, target) {
 }
 
 twoSumm([3, 2, 4], 6);
+///////////////
+console.log(
+  // Входной массив
+  [1, 4, 3, 0, 4, 5, 4]
+    // Оставляем только чётные числа
+    .filter((element) => !(element % 2))
+    // Считаем квадратный корень и записываем в аккумулятор
+    .reduceRight((accumulator, element) => accumulator + Math.sqrt(element), 0)
+); // 6
