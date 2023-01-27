c8b41cf93bf1b479cd44fdeb344f947392774f0b
Evhen Pavelko <lomonosov213@i.ua>
2023-01-27 21:33:11 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -146,4 +146,16 @@ console.log(
     .filter((element) => !(element % 2))
     // Считаем квадратный корень и записываем в аккумулятор
     .reduceRight((accumulator, element) => accumulator + Math.sqrt(element), 0)
-); // 6
+); //
+let n = 10;
+
+nextPrime: for (let i = 2; i <= n; i++) {
+  // Для всех i...
+
+  for (let j = 2; j < i; j++) {
+    // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  alert(i); // простое число
+}
