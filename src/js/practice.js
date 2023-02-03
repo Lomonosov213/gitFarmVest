83986d37e1b02dc35fee9e54d588e3051f5e7388
Evhen Pavelko <lomonosov213@i.ua>
2023-02-03 09:09:47 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -222,3 +222,19 @@ if (n < 1) {
 } else {
   alert(pow(x, n));
 }
+///////////////////////
+function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm('А родители разрешили?');
+  }
+}
+
+let age = prompt('Сколько вам лет?', 18);
+
+if ( checkAge(age) ) {
+  alert( 'Доступ получен' );
+} else {
+  alert( 'Доступ закрыт' );
+}
