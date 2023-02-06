16a503f782d801efa164a799b01e7ec9c28db0f1
Evhen Pavelko <lomonosov213@i.ua>
2023-02-06 17:52:09 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -4,3 +4,12 @@ function doNothing() {
 
 alert(doNothing() === undefined); // true
 ////////
+function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    alert(i); // простое
+  }
+}
