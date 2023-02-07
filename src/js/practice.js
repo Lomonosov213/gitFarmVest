a3cc4b1b83cad10fe9f525c73e6a56390cec161d
Evhen Pavelko <lomonosov213@i.ua>
2023-02-07 18:21:18 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -13,3 +13,19 @@ function showPrimes(n) {
     alert(i); // простое
   }
 }
+////////////////////
+function showPrimes(n) {
+  for (let i = 2; i < n; i++) {
+    if (!isPrime(i)) continue;
+
+    alert(i); // простое
+  }
+}
+
+function isPrime(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
+}
+/////////////////////
