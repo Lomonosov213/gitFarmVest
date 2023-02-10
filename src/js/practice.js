1c56be90813279775598dda15700c4e82d758d7a
Evhen Pavelko <lomonosov213@i.ua>
2023-02-10 19:04:01 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -53,3 +53,32 @@ function min(a, b) {
 function min(a, b) {
   return a < b ? a : b;
 }
+//////////
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", '');
+let n = prompt("n?", '');
+
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+  alert( pow(x, n) );
+}
+////////////////
+let degree = ( x,n ) =>{
+x = prompt("x", '2')
+n = prompt("n", '2')
+if( n % 1 !== 0){
+alert('only normal number mathafucker')
+}
+return x ** n
+}
+console.log(degree(2,4))
