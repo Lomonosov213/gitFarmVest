b587edb256fe3cf3a7574188e26c3d4c4652f5dd
Evhen Pavelko <lomonosov213@i.ua>
2023-02-19 11:16:06 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -8,3 +8,22 @@ try {
   alert("JSON Error: " + err); // JSON Error: ReferenceError: user is not defined
   // (не JSON ошибка на самом деле)
 }
+///////////////////////
+let son = '{ "age": 30 }'; // данные неполны
+try {
+  let user = JSON.parse(json);
+
+  if (!user.name) {
+    throw new SyntaxError("Данные неполны: нет имени");
+  }
+
+  blabla(); // неожиданная ошибка
+
+  alert(user.name);
+} catch (e) {
+  if (e.name == "SyntaxError") {
+    alert("JSON Error: " + e.message);
+  } else {
+    throw e; // проброс (*)
+  }
+}
