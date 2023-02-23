6b0845f1ae60aa2fcf1bd707f48717b8b3d55333
Evhen Pavelko <lomonosov213@i.ua>
2023-02-23 18:04:49 +0100

    february

--- src/js/practice.js
+++ src/js/practice.js
@@ -91,3 +91,22 @@ function func() {
 }
 
 alert(func());
+////////////////
+class ValidationError extends Error {
+  constructor(message) {
+    super(message); // (1)
+    this.name = "ValidationError"; // (2)
+  }
+}
+
+function test() {
+  throw new ValidationError("Упс!");
+}
+
+try {
+  test();
+} catch (err) {
+  alert(err.message); // Упс!
+  alert(err.name); // ValidationError
+  alert(err.stack); // список вложенных вызовов с номерами строк для каждого
+}
