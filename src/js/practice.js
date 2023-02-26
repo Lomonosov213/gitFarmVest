417198f5d7dcf7dacb2b1a018e9c3f0e97a1c873
Evhen Pavelko <lomonosov213@i.ua>
2023-02-26 13:03:21 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -61,3 +61,64 @@ class PropertyRequiredError extends ValidationError {
 
 // name корректное
 alert(new PropertyRequiredError("field").name);
+//////////////////
+class ReadError extends Error {
+  constructor(message, cause) {
+    super(message);
+    this.cause = cause;
+    this.name = "ReadError";
+  }
+}
+
+class ValidationError extends Error {
+  /*...*/
+}
+class PropertyRequiredError extends ValidationError {
+  /* ... */
+}
+
+function validateUser(user) {
+  if (!user.age) {
+    throw new PropertyRequiredError("age");
+  }
+
+  if (!user.name) {
+    throw new PropertyRequiredError("name");
+  }
+}
+
+function readUser(json) {
+  let user;
+
+  try {
+    user = JSON.parse(json);
+  } catch (err) {
+    if (err instanceof SyntaxError) {
+      throw new ReadError("Синтаксическая ошибка", err);
+    } else {
+      throw err;
+    }
+  }
+
+  try {
+    validateUser(user);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      throw new ReadError("Ошибка валидации", err);
+    } else {
+      throw err;
+    }
+  }
+}
+
+try {
+  readUser("{bad json}");
+} catch (e) {
+  if (e instanceof ReadError) {
+    alert(e);
+    // Исходная ошибка: SyntaxError:Unexpected token b in JSON at position 1
+    alert("Исходная ошибка: " + e.cause);
+  } else {
+    throw e;
+  }
+}
