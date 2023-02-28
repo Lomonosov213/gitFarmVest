b8e73a71e30a493d128794feeef1d23b5f7d509e
Evhen Pavelko <lomonosov213@i.ua>
2023-02-28 09:34:41 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -159,3 +159,23 @@ async function showAvatar() {
 }
 
 showAvatar();
+//////////////
+class Thenable {
+  constructor(num) {
+    this.num = num;
+  }
+  then(resolve, reject) {
+    alert(resolve);
+    // выполнить resolve со значением this.num * 2 через 1000мс
+    setTimeout(() => resolve(this.num * 2), 1000); // (*)
+  }
+}
+
+async function f() {
+  // код будет ждать 1 секунду,
+  // после чего значение result станет равным 2
+  let result = await new Thenable(1);
+  alert(result);
+}
+
+f();
