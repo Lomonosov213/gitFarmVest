1e42271c2b51638c88c0a8376a4b6400cd514b4d
Evhen Pavelko <lomonosov213@i.ua>
2023-03-01 12:56:02 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -187,3 +187,13 @@ class Waiter {
 }
 
 new Waiter().wait().then(alert); // 1
+///////////
+async function f() {
+  try {
+    let response = await fetch("http://no-such-url");
+  } catch (err) {
+    alert(err); // TypeError: failed to fetch
+  }
+}
+
+f();
