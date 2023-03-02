b8ba0ac11b1eac14cf32e4813e1fcbd025d0aa30
Evhen Pavelko <lomonosov213@i.ua>
2023-03-02 09:50:41 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -197,3 +197,17 @@ async function f() {
 }
 
 f();
+/////////////////
+async function loadJson(url) {
+  // (1)
+  let response = await fetch(url); // (2)
+
+  if (response.status == 200) {
+    let json = await response.json(); // (3)
+    return json;
+  }
+
+  throw new Error(response.status);
+}
+
+loadJson("no-such-user.json").catch(alert); // Error: 404 (4)
