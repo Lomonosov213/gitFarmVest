50c1c17ff8cbe6b4e40fdde405509e404d97ce7d
Evhen Pavelko <lomonosov213@i.ua>
2023-03-05 00:22:54 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -270,3 +270,16 @@ async function demoGithubUser() {
 
 demoGithubUser();
 ////////////////
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  // покажет 10 через 1 секунду
+  wait().then(result => alert(result));
+}
+
+f();
+////////////////////
