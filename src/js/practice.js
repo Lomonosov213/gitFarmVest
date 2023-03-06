f07da2c0b150122019df62d7440fb97bab27fcaa
Evhen Pavelko <lomonosov213@i.ua>
2023-03-06 17:49:55 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -283,3 +283,21 @@ function f() {
 
 f();
 ////////////////////
+function* gen() {
+  let ask1 = yield "2 + 2 = ?";
+
+  alert(ask1); // 4
+
+  let ask2 = yield "3 * 3 = ?"
+
+  alert(ask2); // 9
+}
+
+let generator = gen();
+
+alert( generator.next().value ); // "2 + 2 = ?"
+
+alert( generator.next(4).value ); // "3 * 3 = ?"
+
+alert( generator.next(9).done );
+///////////////
