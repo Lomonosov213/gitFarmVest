9be2fa83d5a79f651b121708e791666a7afdb368
Evhen Pavelko <lomonosov213@i.ua>
2023-03-07 19:10:19 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -330,3 +330,18 @@ try {
 } catch (e) {
   alert(e); // покажет ошибку
 }
+/////////////
+function* pseudoRandom(seed) {
+  let value = seed;
+
+  while (true) {
+    value = (value * 16807) % 2147483647;
+    yield value;
+  }
+}
+
+let generator = pseudoRandom(1);
+
+alert(generator.next().value); // 16807
+alert(generator.next().value); // 282475249
+alert(generator.next().value); // 1622650073
