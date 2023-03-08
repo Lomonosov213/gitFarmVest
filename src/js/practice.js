33f01778c4b8b6223718a50dcdb2e7c01384679a
Evhen Pavelko <lomonosov213@i.ua>
2023-03-08 19:00:21 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -346,3 +346,26 @@ alert(generator.next().value); // 16807
 alert(generator.next().value); // 282475249
 alert(generator.next().value); // 1622650073
 ///////////////
+async function* fetchCommits(repo) {
+  let url = `https://api.github.com/repos/${repo}/commits`;
+
+  while (url) {
+    const response = await fetch(url, {
+      // (1)
+      headers: { "User-Agent": "Our script" }, // GitHub требует заголовок user-agent
+    });
+
+    const body = await response.json(); // (2) ответ в формате JSON (массив коммитов)
+
+    // (3) Ссылка на следующую страницу находится в заголовках, извлекаем её
+    let nextPage = response.headers.get("Link").match(/<(.*?)>; rel="next"/);
+    nextPage = nextPage && nextPage[1];
+
+    url = nextPage;
+
+    for (let commit of body) {
+      // (4) вернуть коммиты один за другим, до окончания страницы
+      yield commit;
+    }
+  }
+}
