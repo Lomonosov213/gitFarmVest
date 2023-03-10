64ee2d7317fb5b4252e333b796063b2c328efd6c
Evhen Pavelko <lomonosov213@i.ua>
2023-03-10 17:46:21 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -392,3 +392,14 @@ export class User {
     this.name = name;
   }
 }
+////////
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+}
+
+let user = makeUser();
+
+alert( user.ref.name );
