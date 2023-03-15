9362521ac22fdc592a474314048470dc195a8c48
Evhen Pavelko <lomonosov213@i.ua>
2023-03-15 10:22:56 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -508,3 +508,16 @@ calculator.read();
 
 alert("Sum=" + calculator.sum());
 alert("Mul=" + calculator.mul());
+///////////////
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    this.value += +prompt("Сколько нужно добавить?", 0);
+  };
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
