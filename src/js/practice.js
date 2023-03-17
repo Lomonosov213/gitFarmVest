5e760acd75abe584be895557ae98522863b5aff6
Evhen Pavelko <lomonosov213@i.ua>
2023-03-17 10:30:41 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -542,3 +542,22 @@ calculator.read();
 
 alert("Sum=" + calculator.sum());
 alert("Mul=" + calculator.mul());
+//////////////////
+function pow(x, n) {
+  let result = x;
+
+  for (let i = 1; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = prompt("x?", "");
+let n = prompt("n?", "");
+
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+  alert(pow(x, n));
+}
