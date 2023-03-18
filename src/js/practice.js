bdf305dada5604a782b4d8a7ef056adafe5669e1
Evhen Pavelko <lomonosov213@i.ua>
2023-03-18 14:16:31 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -572,3 +572,9 @@ ask(
   () => alert("Вы согласились."),
   () => alert("Вы отменили выполнение.")
 );
+////////////
+let user = {};
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
