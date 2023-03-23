3d25c85adf3ad1e113ea89efabf3ed5e0eb16a65
Evhen Pavelko <lomonosov213@i.ua>
2023-03-23 19:20:44 +0100

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -8,3 +8,21 @@ function makeUser() {
 let user = makeUser();
 
 alert(user.ref.name);
+///////////////
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep() {
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep().down().showStep();
