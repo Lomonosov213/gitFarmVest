ced025b4340d8aa1e1ef88e29aa0195c7e7d454f
Evhen Pavelko <lomonosov213@i.ua>
2023-03-25 20:04:47 +0100

    зщые

--- src/js/practice.js
+++ src/js/practice.js
@@ -37,3 +37,16 @@ function B() {
 }
 
 alert(new A() == new B()); // true
+/////////////
+function Accumulator(startingValue) {
+  this.value = startingValue;
+
+  this.read = function () {
+    this.value += +prompt("Сколько нужно добавить?", 0);
+  };
+}
+
+let accumulator = new Accumulator(1);
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
