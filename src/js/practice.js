def37b7ad7ca130cb69d7fc96449f9d1daa1041a
Evhen Pavelko <lomonosov213@i.ua>
2023-03-26 12:26:28 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -50,3 +50,18 @@ let accumulator = new Accumulator(1);
 accumulator.read();
 accumulator.read();
 alert(accumulator.value);
+////////////
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  },
+};
+
+// демонстрация результатов преобразований:
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
