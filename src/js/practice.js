7a3d824c2a9882617178136241ecac5ac39609ff
Evhen Pavelko <lomonosov213@i.ua>
2023-03-28 12:31:27 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -84,3 +84,23 @@ let user2 = {
 alert(user2); // toString -> {name: "John"}
 alert(+user2); // valueOf -> 1000
 alert(user2 + 500); // valueOf -> 1500
+///////////////
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
+
+function bench(f) {
+  let date1 = new Date(0);
+  let date2 = new Date();
+
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) f(date1, date2);
+  return Date.now() - start;
+}
+
+alert("Время diffSubtract: " + bench(diffSubtract) + "мс");
+alert("Время diffGetTime: " + bench(diffGetTime) + "мс");
