58702f479a221c9480079100d4d0dc0f5e962bc8
Evhen Pavelko <lomonosov213@i.ua>
2023-03-29 09:55:37 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -104,3 +104,32 @@ function bench(f) {
 
 alert("Время diffSubtract: " + bench(diffSubtract) + "мс");
 alert("Время diffGetTime: " + bench(diffGetTime) + "мс");
+/////////////////
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
+
+function bench(f) {
+  let date1 = new Date(0);
+  let date2 = new Date();
+
+  let start = Date.now();
+  for (let i = 0; i < 100000; i++) f(date1, date2);
+  return Date.now() - start;
+}
+
+let time1 = 0;
+let time2 = 0;
+
+// bench(diffSubtract) и bench(diffGetTime) поочерёдно запускаются 10 раз
+for (let i = 0; i < 10; i++) {
+  time1 += bench(diffSubtract);
+  time2 += bench(diffGetTime);
+}
+
+alert("Итоговое время diffSubtract: " + time1);
+alert("Итоговое время diffGetTime: " + time2);
