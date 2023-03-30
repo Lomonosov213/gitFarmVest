993394cf5a0a9e9040ec07edfc0001cc44e3247b
Evhen Pavelko <lomonosov213@i.ua>
2023-03-31 00:07:06 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -14,3 +14,14 @@ let date = new Date(2015, 0, 2);
 alert(getDateAgo(date, 1)); // 1, (1 Jan 2015)
 alert(getDateAgo(date, 2)); // 31, (31 Dec 2014)
 alert(getDateAgo(date, 365)); // 2, (2 Jan 2014)
+////////////////
+function getSecondsToTomorrow() {
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let totalSecondsToday = (hour * 60 + minutes) * 60 + seconds;
+  let totalSecondsInADay = 86400;
+
+  return totalSecondsInADay - totalSecondsToday;
+}
