c8d53cfdd6e09372a90aab6c3946d6a547ab03ad
Evhen Pavelko <lomonosov213@i.ua>
2023-04-06 18:20:12 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -17,3 +17,18 @@ alert(sum(1)(2)); // 3
 alert(sum(5)(-1)(2)); // 6
 alert(sum(6)(-1)(-2)(-3)); // 0
 alert(sum(0)(1)(2)(3)(4)(5)); // 15
+///////
+function printNumbers(from, to) {
+  let current = from;
+
+  let timerId = setInterval(function () {
+    alert(current);
+    if (current == to) {
+      clearInterval(timerId);
+    }
+    current++;
+  }, 1000);
+}
+
+// использование:
+printNumbers(5, 10);
