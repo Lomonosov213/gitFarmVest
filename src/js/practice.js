6f7211bb15da6d46a6662033a61b16774e859742
Evhen Pavelko <lomonosov213@i.ua>
2023-04-09 15:43:34 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -68,3 +68,32 @@ function debounce(f, ms) {
     setTimeout(() => (isCooldown = false), ms);
   };
 }
+///////////////
+function throttle(func, ms) {
+  let isThrottled = false,
+    savedArgs,
+    savedThis;
+
+  function wrapper() {
+    if (isThrottled) {
+      // (2)
+      savedArgs = arguments;
+      savedThis = this;
+      return;
+    }
+
+    func.apply(this, arguments); // (1)
+
+    isThrottled = true;
+
+    setTimeout(function () {
+      isThrottled = false; // (3)
+      if (savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+        savedArgs = savedThis = null;
+      }
+    }, ms);
+  }
+
+  return wrapper;
+}
