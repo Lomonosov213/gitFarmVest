ffc501a6e9f89ca5229b4c84621e02b0f054d43d
Evhen Pavelko <lomonosov213@i.ua>
2023-04-11 10:35:16 +0200

    post

--- src/js/practice.js
+++ src/js/practice.js
@@ -125,3 +125,17 @@ let user = {
 };
 
 askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+//////////////
+Function.prototype.defer = function (ms) {
+  let f = this;
+  return function (...args) {
+    setTimeout(() => f.apply(this, args), ms);
+  };
+};
+
+// check it
+function f(a, b) {
+  alert(a + b);
+}
+
+f.defer(1000)(1, 2);
